74633225eaf8d5eba2c5de37d2fed0fd8799ff95
Jose A. Peralta <jperalta3004@gmail.com>
2026-02-12 09:36:15 -0500

    Update script.test.js

--- script.test.js
+++ script.test.js
@@ -1,6 +1,7 @@
-// import { test, expect } from 'vitest'
+import { test, expect } from 'vitest'
 import { sum } from './script.js'
 
 test('suma incorrecta', () => {
     expect(sum(2, 2)).toBe(5);  // Este test fallará intencionalmente
+
 });
