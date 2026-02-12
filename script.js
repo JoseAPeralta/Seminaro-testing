952eba1192b7083e77f558c33516548942f910f9
Jose A. Peralta <jperalta3004@gmail.com>
2026-02-12 08:56:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-export function add(a, b) {
-    return a + b;
+// script.js
+export function sum(a, b) {
+    return a + b    // COMENTARIO_ERROR: falta punto y coma (lint)
 }
 
-console.log(add(2, 3));
+const user = "<img src=x onerror=alert('XSS')>";
+// COMENTARIO_ERROR: 'innerHTML' con HTML no sanitizado -> posible vulnerabilidad XSS【24†L279-L287】
+document.getElementById("welcome").innerHTML = "Hola, " + user;
